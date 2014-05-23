c0d8e15e499f7803825f6fd602c8da21d05fb143
Sergey Ponomarev <stokito@gmail.com>
2014-05-23 20:49:31 +0300

    reformat Setting.groovy

--- grails-app/domain/org/grails/plugins/settings/Setting.groovy
+++ grails-app/domain/org/grails/plugins/settings/Setting.groovy
@@ -1,16 +1,17 @@
 package org.grails.plugins.settings
 
 import org.codehaus.groovy.grails.commons.ConfigurationHolder
-import java.text.SimpleDateFormat
 import java.text.ParseException
-import org.grails.plugins.settings.*
+import java.text.SimpleDateFormat
 
 class Setting {
     private static loaded = false
-    private static cache = new LinkedHashMap((int) 16, (float) 0.75, (boolean) true)
-    private static long maxCacheSize = 8L * 1024L // Cache size in KB (default is 8kb)
+    private static cache = new LinkedHashMap(16, 0.75F, true)
+    /** Cache size in KB (default is 8kb) */
+    private static long maxCacheSize = 8L * 1024L
     private static long currentCacheSize = 0L
-    private static final missingValue = "\b" // an impossible value signifying that no such code exists in the database
+    /** an impossible value signifying that no such code exists in the database */
+    private static final missingValue = "\b"
     private static long cacheHits = 0L
     private static long cacheMisses = 0L
 
@@ -22,25 +23,24 @@ class Setting {
 
     static mapping = {
         columns {
-            code index: "setting_code_idx"
+            code index: 'setting_code_idx'
         }
     }
 
     static constraints = {
-        code(blank:false, size: 1..100, unique: true)
-        type(blank:false, inList: ['string', 'integer', 'decimal', 'date'])
-        value(blank:false, size: 1..100, validator: {val, obj -> (Setting.decodeValue(obj.type, val) != null) ? true : false})
+        code(blank: false, size: 1..100, unique: true)
+        type(blank: false, inList: ['string', 'integer', 'decimal', 'date'])
+        value(blank: false, size: 1..100, validator: { val, obj ->
+            Setting.decodeValue(obj.type, val) != null })
     }
 
     static valueFor(String code) {
         if (!loaded) load()
         if (!code) return null
-
         def val
         if (maxCacheSize > 0) {
-            synchronized(cache) {
+            synchronized (cache) {
                 val = cache.get(code)
-
                 if (val) {
                     cacheHits++
                 } else {
@@ -54,21 +54,15 @@ class Setting {
             if (val) {
                 val = Setting.decodeValue(val.type, val.value)
             }
-
             if (!val) val = missingValue
-
             if (maxCacheSize > 0) {
                 synchronized (cache) {
-
                     // Put it in the cache
                     def prev = cache.put(code, val)
-
                     // Another user may have inserted it while we weren't looking
                     if (prev != null) currentCacheSize -= code.length() + valSize(prev)
-
                     // Increment the cache size with our data
                     currentCacheSize += code.length() + valSize(val)
-
                     // Adjust the cache size if required
                     if (currentCacheSize > maxCacheSize) {
                         def entries = cache.entrySet().iterator()
@@ -82,18 +76,16 @@ class Setting {
                 }
             }
         }
-
         return (val != missingValue) ? val : null
     }
 
     static valueFor(String code, Object dflt) {
         def val = valueFor(code)
-
         return (val != null) ? val : dflt
     }
 
     static resetAll() {
-        synchronized(cache) {
+        synchronized (cache) {
             cache.clear()
             currentCacheSize = 0L
             cacheHits = 0L
@@ -102,7 +94,7 @@ class Setting {
     }
 
     static resetThis(String code) {
-        synchronized(cache) {
+        synchronized (cache) {
             def val = cache.remove(code)
             if (val) {
                 currentCacheSize -= code.length() + valSize(val)
@@ -112,10 +104,9 @@ class Setting {
 
     static load() {
         def size = ConfigurationHolder.config.settings.cache.size.kb
-        if (size != null && size instanceof Integer && size >= 0 && size <= 1024 * 1024) {
+        if (size && size instanceof Integer && size >= 0 && size <= 1024 * 1024) {
             maxCacheSize = size * 1024L
         }
-
         loaded = true
     }
 
@@ -123,30 +114,29 @@ class Setting {
         if (val) {
             switch (type) {
                 case "integer":
-                try {
-                    return new Integer(val)
-                } catch (NumberFormatException ne) {}
-                break
-
+                    try {
+                        return new Integer(val)
+                    } catch (NumberFormatException ne) {
+                    }
+                    break
                 case "decimal":
-                try {
-                    return new BigDecimal(val)
-                } catch (NumberFormatException ne) {}
-                break
-
+                    try {
+                        return new BigDecimal(val)
+                    } catch (NumberFormatException ne) {
+                    }
+                    break
                 case "date":
-                try {
-                    def fmt = (val.length() == 10) ? "yyyy-MM-dd" : "yyyy-MM-dd HH:mm"
-                    return new SimpleDateFormat(fmt, Locale.US).parse(val)
-                } catch (ParseException pe) {}
-                break
-
+                    try {
+                        def fmt = (val.length() == 10) ? "yyyy-MM-dd" : "yyyy-MM-dd HH:mm"
+                        return new SimpleDateFormat(fmt, Locale.US).parse(val)
+                    } catch (ParseException pe) {
+                    }
+                    break
                 default:  // string
-                return val
-                break
+                    return val
+                    break
             }
         }
-
         return null
     }
 
@@ -154,23 +144,18 @@ class Setting {
         if (val == null) {
             return 0
         }
-
         if (val instanceof String) {
             return val.length()
         }
-
         if (val instanceof Integer) {
             return 4
         }
-
         if (val instanceof BigDecimal) {
             return 8
         }
-
         if (val instanceof Date) {
             return 8
         }
-
         return 8    // A reasonable default
     }
 
@@ -183,7 +168,6 @@ class Setting {
             stats.hits = cacheHits
             stats.misses = cacheMisses
         }
-
         return stats
     }
 }

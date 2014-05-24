fab12979855fff2a0d29abf9f6306bcb02da8e7a
Sergey Ponomarev <stokito@gmail.com>
2014-05-24 05:19:25 +0300

    SettingsGrailsPlugin.groovy

--- SettingsGrailsPlugin.groovy
+++ SettingsGrailsPlugin.groovy
@@ -1,10 +1,7 @@
 class SettingsGrailsPlugin {
-    def version = "1.5-SNAPSHOT"
+    def version = "1.6-SNAPSHOT"
     String grailsVersion = '2.0.0 > *'
-
     def dependsOn = [:]
-
-    // TODO Fill in these fields
     def author = "Paul Fernley"
     def authorEmail = "paul@pfernley.orangehome.co.uk"
     def title = "Application settings (global constants) plugin"
@@ -16,34 +13,17 @@ Create views are included and, after installing the plugin, will be available at
 a URL such as http://myServer/myApp/setting/list etc. These views assume you are
 using a layout called 'main'.
 '''
-
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/Settings+Plugin"
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
+    def documentation = "http://grails.org/plugin/settings"
+    // License: one of 'APACHE', 'GPL2', 'GPL3'
+    def license = "APACHE"
+    // Any additional developers beyond the author specified above.
+    def developers = [
+            [ name: "Paul Fernley", email: "paul@pfernley.orangehome.co.uk" ],
+            [ name: "Stéphane Prohaszka" ],
+            [ name: "Sergey  Ponomarev", email: "stokito@gmail.com" ]
+    ]
+    // Location of the plugin's issue tracker.
+    def issueManagement = [ system: "GITHUB", url: "https://github.com/sprohaszka/grails-settings/issues" ]
+    // Online location of the plugin's browseable source code.
+    def scm = [ url: "https://github.com/sprohaszka/grails-settings" ]
 }
